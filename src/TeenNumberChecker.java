6517859b23421771f63108f46ab75d2bc6807e88
ryoray <praretno.wibowo@gmail.com>
2020-04-16 15:22:47 +0700

    Initial commit

---
+++ src/TeenNumberChecker.java
@@ -0,0 +1,16 @@
+public class TeenNumberChecker {
+
+    public static boolean hasTeen (int firstNumber, int secondNumber, int thirdNumber) {
+        if (( firstNumber >= 13 && firstNumber <= 19) || (secondNumber >= 13 && secondNumber <= 19) ||
+                (thirdNumber >= 13 && thirdNumber <= 19)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isTeen (int number) {
+        if (number >= 13 && number <= 19) {
+            return true;
+        } return false;
+    }
+}
